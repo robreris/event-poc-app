abf284bccc175169cfa95f0a2e84b52ff1a2a3f6
Robert Reris <rreris@fortinet.com>
2025-04-03 16:58:14 -0700

    testing

--- app/app.py
+++ app/app.py
@@ -7,6 +7,13 @@ import socket
 
 JOB_ID = "job-123"
 ROLE = os.getenv("ROLE", "producer")
+print(f"######Role is: {ROLE}")
+
+def ensure_path(job_id, role):
+    path = f"/artifacts/{job_id}/{role}"
+    os.makedirs(path, exist_ok=True)
+    with open(f"{path}/output.txt", "w") as f:
+        f.write(f"{role} completed at {time.time()}")
 
 host = os.environ.get("RABBIT_HOST", "rabbitmq")
 credentials = pika.PlainCredentials("user", "bitnami")
@@ -22,8 +29,6 @@ for attempt in range(10):
 else:
     raise Exception("Failed to connect to RabbitMQ after 10 tries")
 
-conn = pika.BlockingConnection(params)
-channel = conn.channel()
 channel.queue_declare(queue='ppt-uploaded')
 channel.queue_declare(queue='tts-complete')
 channel.queue_declare(queue='rendering-complete')
@@ -44,6 +49,7 @@ elif ROLE == "tts":
         time.sleep(2)
         done = { "event": "tts-complete", "job_id": msg["job_id"] }
         channel.basic_publish(exchange='', routing_key='tts-complete', body=json.dumps(done))
+        ensure_path(msg["job_id"], "tts")
         print("TTS: Sent tts-complete")
 
     channel.basic_consume(queue='ppt-uploaded', on_message_callback=callback, auto_ack=True)
@@ -56,6 +62,7 @@ elif ROLE == "renderer":
         time.sleep(3)
         done = { "event": "rendering-complete", "job_id": msg["job_id"] }
         channel.basic_publish(exchange='', routing_key='rendering-complete', body=json.dumps(done))
+        ensure_path(msg["job_id"], "renderer")
         print("Renderer: Sent rendering-complete")
 
     channel.basic_consume(queue='ppt-uploaded', on_message_callback=callback, auto_ack=True)
